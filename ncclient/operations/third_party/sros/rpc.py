3aafd84ce1929c6a11d0abff22e5ce6099e3ee72
Thomas Hendriks <hendriks_thomas@hotmail.com>
2022-11-07 13:13:40 +0100

    Adding md-compare for sros 3rd party as an operation. First version.

--- ncclient/operations/third_party/sros/rpc.py
+++ ncclient/operations/third_party/sros/rpc.py
@@ -24,3 +24,57 @@ class MdCliRawCommand(RPC):
         sub_ele(raw_cmd_node, 'md-cli-input-line').text = command
         self._huge_tree = True
         return self._request(node)
+
+
+class MdCompare(RPC):
+    def _handle_target(self, target_node, target_node_type, target_node_value):
+        if target_node_type == "configuration_region":
+            sub_ele(target_node, target_node_value)
+        elif target_node_type == "url":
+            sub_ele(target_node, 'url').text = target_node_value
+        elif target_node_type == "rollback":
+            rollback_node = sub_ele(target_node, "rollback")
+            sub_ele(rollback_node, "checkpoint-id").text = target_node_value
+        else:
+            assert False, f"{target_node_type} is not a known type."
+
+    def _handle_path(self, md_compare_node, path):
+        if path != '/':
+            path_node = sub_ele(md_compare_node, 'path')
+            subtree_path_node = sub_ele(path_node, 'subtree-path')
+            append_node = subtree_path_node
+            if path[0] == '/': path = path[1:]
+            if path.split('/')[0] == "configure":
+                append_node = sub_ele(append_node, 'configure', attrs={"xmlns":"urn:nokia.com:sros:ns:yang:sr:conf"} )
+            else:
+                assert False, f"{path.split('/')[0]}"
+            for i in path.split('/')[1:-1]:
+                if i:
+                    append_node = sub_ele(append_node, i)
+            # just to have a <tag></tag>, otherwise its going to be <tag/>
+            sub_ele(append_node, path.split('/')[-1]).text = ""
+
+    def request(self,   src="baseline",
+                        src_type = "configuration_region",
+                        dst="candidate",
+                        dst_type = "url",
+                        response_format='xml',
+                        configuration_region = "configure",
+                        path='/'):
+        assert response_format in ["xml", "md-cli"], "Must use one of xml, json or md-cli for format. Default is 'xml'."
+        node, cmp_node = global_operations('md-compare')
+        
+        sub_ele(cmp_node, 'format').text = response_format
+        
+        source = sub_ele(cmp_node, 'source')
+        destination = sub_ele(cmp_node, 'destination')
+
+        self._handle_target(source, src_type, src)
+        self._handle_target(destination, dst_type, dst)
+
+        self._handle_path(cmp_node, path)
+
+        sub_ele(cmp_node, 'configuration-region').text = configuration_region
+
+        self._huge_tree = True
+        return self._request(node)
